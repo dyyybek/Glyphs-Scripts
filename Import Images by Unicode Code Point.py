c48543a0c2ee7bafb3df792ccd725fb63f1eefa5
dyyybek <marcin.dybas@me.com>
2015-11-07 14:55:10 +0100

    Current Glyph Master
    There's no more need to predefine the layer into which we'd like to
    import the images. Instead images are imported into currently selected
    master. Huge thanks to Georg Seifert. Discussed @
    https://forum.glyphsapp.com/t/import-many-images-at-once/3315

--- Import Images by Unicode Code Point.py
+++ Import Images by Unicode Code Point.py
@@ -1,18 +1,15 @@
 #MenuTitle: Import Images by Unicode Code Point
 # -*- coding: utf-8 -*-
 __doc__="""
-Imports images pre-named with unicode code point into a coresponding Glyph and Master
+Imports images pre-named with unicode code point into a coresponding Glyph and currently selected Master
 """
+import GlyphsApp
 
-picklayer = 'Light Italic'
-pickpath = '/Users/dyb/Desktop/'
+pickpath = GetFolder("Please choose a folder with images") + '/'
 pickextension = 'png'
-masterID = None
-for master in Glyphs.font.masters:
-    if master.name == picklayer:
-        masterID = master.id
+masterID = Glyphs.font.masters[Glyphs.font.masterIndex].id
 for glyph in Glyphs.font.glyphs:
-    if glyph.unicode:
-        print glyph.unicode
-        layer = glyph.layers[masterID]
-        layer.backgroundImage = GSBackgroundImage(pickpath + glyph.unicode + '.' + pickextension)
+	if glyph.unicode:
+		print glyph.unicode
+		layer = glyph.layers[masterID]
+		layer.backgroundImage = GSBackgroundImage(pickpath + glyph.unicode + '.' + pickextension)
